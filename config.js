a316d5f0761b8b693c4def2916def0c98dc1797a
autjsmo <auti1175@virgilio.it>
2026-02-17 17:46:33 +0100

    Update ORDERS_SITE_BASE URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// Configurazione API
 const CONFIG = {
-  API_BASE: 'https://qr-ordini-api.auti1175.workers.dev', // ← CAMBIA con il tuo URL Worker
-  ORDERS_SITE_BASE: 'https://auti1175.github.io/bar-ordini/' // ← CAMBIA con il tuo URL sito ordini
+  API_BASE: 'https://qr-ordini-api.auti1175.workers.dev',
+  ORDERS_SITE_BASE: 'https://autjsmo.github.io/bar-ordini/'
 };
